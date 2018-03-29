70ef6a3d0845f2b9dbf35fdb0a432e61767ea968
zhaojianxin <215696013@qq.com>
2018-03-29 22:42:46 +0800

    兼容多种引入方式

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 
 /**
  * 根据设计稿的宽度和设备宽度动态设置跟字体的大小
- * @param {*} designWith 
+ * 
  */
-export default function setRem(designWith){
-    const resizeEvent= 'orientationchange' in window ? 'orientationchange' : 'resize';
-    const recalc = () => {
-      const docEl = document.documentElement;
-      const { clientWidth } = docEl;
-      if (!clientWidth) return;
-      docEl.style.fontSize = `${100 * (designWith / 1080)}px`;
-    };
-  
-    if (!document.addEventListener) return;
-    window.addEventListener(resizeEvent, recalc, false);
-    document.addEventListener('DOMContentLoaded', recalc, false);
-}
+
+(function (global, factory) {
+typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+typeof define === 'function' && define.amd ? define(factory) : (global.setRem = factory());
+})(this, function () {
+    function setRem(designWith){
+        if(typeof window !== 'undefined' && typeof document !== 'undefined'){
+            const resizeEvent= 'orientationchange' in window ? 'orientationchange' : 'resize';
+            const recalc = () => {
+              const docEl = document.documentElement;
+              const { clientWidth } = docEl;
+              if (!clientWidth) return;
+              docEl.style.fontSize = `${100 * (designWith / 1080)}px`;
+            };
+          
+            if (!document.addEventListener) return;
+            window.addEventListener(resizeEvent, recalc, false);
+            document.addEventListener('DOMContentLoaded', recalc, false);
+        }
+    }
+    return setRem;
+});
